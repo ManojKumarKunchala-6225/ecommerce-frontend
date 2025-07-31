044357404d7817b871d583d1260f227e6b0eae7c
ManojKumarKunchala-6225 <manojkumark6225@gmail.com>
2025-07-31 22:40:34 +0530

    Second Step Completed

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,9 @@
 console.log("E-Commerce Website Loaded");
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+// Toggle mobile menu and hamburger animation
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  hamburger.classList.toggle("open");
+});
